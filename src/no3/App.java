7cefa74ada9d417b1d0bc47668174e046136261b
pierreviences <imaniyazid@gmail.com>
2023-11-14 16:36:49 +0700

    feat: add soal no 3

--- src/no3/App.java
+++ src/no3/App.java
@@ -1,6 +1,10 @@
 package no3;
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        int i = 5;
+        while(i>0){
+            System.out.print(i);
+            i--;
+        }
     }
 }
