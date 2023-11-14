4780423d3c3f08e1ad37db0936b58efea49d31b5
pierreviences <imaniyazid@gmail.com>
2023-11-14 16:31:13 +0700

    add: Soal no 1

---
+++ src/no1/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int total = 0;
+        for (int i = 1; i<=5; i++) {
+            if((i%2)==0){
+              total+=10;
+            } else{
+             total-=5;
+            }
+        }
+        System.out.println(total);
+    }
+}
